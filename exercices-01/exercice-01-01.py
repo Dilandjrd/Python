479f9e0d7803d6bdbc376c35bf3081ecfed31253
Dilan <dilan.djrd@gmail.com>
2022-12-06 11:03:11 +0100

    Ajout des exercices 1, 2 et 3
    Fait les exos 1.1 et 1.2

---
+++ exercices-01/exercice-01-01.py
@@ -0,0 +1,5 @@
+# exo 1.1
+# Affichez le message "programme démarré" en utilisant des doubles quotes
+
+# réponse 1.1
+print("programme démarré")
---
+++ exercices-01/exercice-01-02.py
@@ -0,0 +1,5 @@
+# exo 1.2
+# Affichez le message "programme terminé" en utilisant des simples quotes
+
+# réponse 1.2
+print('programme terminé')
